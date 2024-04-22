074e5258d07eccf309c241245bdc2267c08595fb
HollyGrahamPlank <hollygrahamplank@gmail.com>
2024-04-22 11:59:07 -0500

    Added routes

--- apps/express-backend/src/controllers/index.controllers.ts
+++ apps/express-backend/src/controllers/index.controllers.ts
@@ -1,6 +1,10 @@
 import { RequestHandler } from "express";
 
-export const create: RequestHandler = async (req, res) => {};
+export const create: RequestHandler = async (req, res) => {
+  console.log(`CREATE:\n\t${req.body}`);
+};
+
+export const readLatest: RequestHandler = async (req, res) => {};
 
 export const read: RequestHandler = async (req, res) => {
   res.status(200).send("Hello World!");
--- apps/express-backend/src/routes/index.routes.ts
+++ apps/express-backend/src/routes/index.routes.ts
@@ -3,6 +3,10 @@ import * as controller from "../controllers/index.controllers.js";
 
 const indexRouter: Router = Router();
 
-indexRouter.get("/", controller.read);
+indexRouter.get("/", controller.readLatest);
+indexRouter.post("/", controller.create);
+indexRouter.get("/:id", controller.read);
+indexRouter.patch("/:id", controller.update);
+indexRouter.delete("/:id", controller.remove);
 
 export default indexRouter;

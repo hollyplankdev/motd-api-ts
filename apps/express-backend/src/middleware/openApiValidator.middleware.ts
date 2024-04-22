e79b5786711da80565b7f8b829af2edf18a1618e
HollyGrahamPlank <hollygrahamplank@gmail.com>
2024-04-22 11:46:56 -0500

    Fixed unknown format from mongo-objectid

--- apps/express-backend/src/middleware/openApiValidator.middleware.ts
+++ apps/express-backend/src/middleware/openApiValidator.middleware.ts
@@ -1,4 +1,5 @@
 import * as OpenAPIValidator from "express-openapi-validator";
+import mongoose from "mongoose";
 
 /**
  * @param apiSpecPath Where the OpenAPI yaml specification is on disk.
@@ -11,5 +12,17 @@ export default function create(apiSpecPath: string) {
       removeAdditional: "all",
     },
     validateResponses: true,
+
+    /**
+     * Tell OpenAPIValidator that the custom "mongo-objectid" format is a real thing that it can
+     * handle
+     */
+    serDes: [
+      {
+        format: "mongo-objectid",
+        deserialize: (s: string) => mongoose.Types.ObjectId.createFromHexString(s),
+        serialize: (o: mongoose.Types.ObjectId) => o.toHexString(),
+      },
+    ],
   });
 }

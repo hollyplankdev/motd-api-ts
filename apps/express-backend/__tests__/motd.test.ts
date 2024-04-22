838ad5cb4c4e44bff7fbec34b5bdc1b6de69f562
HollyGrahamPlank <hollygrahamplank@gmail.com>
2024-04-22 16:21:02 -0500

    Testing get latest

--- apps/express-backend/__tests__/motd.test.ts
+++ apps/express-backend/__tests__/motd.test.ts
@@ -2,7 +2,7 @@ import { faker } from "@faker-js/faker";
 import { MessageOfTheDay } from "@motd-ts/models";
 import mongoose from "mongoose";
 import { afterEach, beforeEach, describe, expect, test } from "vitest";
-import { createMotd, fetchMotd } from "../src/services/motd.services";
+import { createMotd, fetchLatestMotd, fetchMotd } from "../src/services/motd.services";
 import TestApp from "./utils/testApp";
 
 describe("Message of the Day", () => {
@@ -54,15 +54,43 @@ describe("Message of the Day", () => {
         expect(motd).toBeFalsy();
       });
 
-      test("valid motd", async () => {
-        const message = faker.company.catchPhrase();
-        const newMotd = (await createMotd(message)) as MessageOfTheDay;
-        const foundMotd = await fetchMotd(newMotd._id);
-        expect(foundMotd).toBeTruthy();
-        expect(foundMotd?._id).toEqual(newMotd._id);
-        expect(foundMotd?.message).toEqual(newMotd.message);
-        expect(foundMotd?.createdAt).toEqual(newMotd.createdAt);
-        expect(foundMotd?.updatedAt).toEqual(newMotd.updatedAt);
+      test("many valid motds", async () => {
+        for (let x = 0; x < 100; x += 1) {
+          // eslint-disable-next-line no-await-in-loop
+          const newMotd = (await createMotd(faker.company.catchPhrase())) as MessageOfTheDay;
+          // eslint-disable-next-line no-await-in-loop
+          const foundMotd = await fetchMotd(newMotd._id);
+          expect(foundMotd).toBeTruthy();
+          expect(foundMotd?._id).toEqual(newMotd._id);
+          expect(foundMotd?.message).toEqual(newMotd.message);
+          expect(foundMotd?.createdAt).toEqual(newMotd.createdAt);
+          expect(foundMotd?.updatedAt).toEqual(newMotd.updatedAt);
+        }
+      });
+    });
+
+    describe("get latest", async () => {
+      test("valid latest", async () => {
+        const newMotd = await createMotd(faker.company.catchPhrase());
+        const latestMotd = await fetchLatestMotd();
+
+        expect(latestMotd).toBeTruthy();
+        expect(latestMotd?._id).toEqual(newMotd?._id);
+        expect(latestMotd?.message).toEqual(newMotd?.message);
+        expect(latestMotd?.createdAt).toEqual(newMotd?.createdAt);
+        expect(latestMotd?.updatedAt).toEqual(newMotd?.updatedAt);
+      });
+
+      test("out of date", async () => {
+        const newMotd = await createMotd(faker.company.catchPhrase());
+        await createMotd(faker.company.catchPhrase());
+        const latestMotd = await fetchLatestMotd();
+
+        expect(latestMotd).toBeTruthy();
+        expect(latestMotd?._id).not.toEqual(newMotd?._id);
+        expect(latestMotd?.message).not.toEqual(newMotd?.message);
+        expect(latestMotd?.createdAt).not.toEqual(newMotd?.createdAt);
+        expect(latestMotd?.updatedAt).not.toEqual(newMotd?.updatedAt);
       });
     });
   });

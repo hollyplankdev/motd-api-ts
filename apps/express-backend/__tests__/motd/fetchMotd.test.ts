864eab670ba5e4a38e098e1a7850bee29e07adb2
HollyGrahamPlank <hollygrahamplank@gmail.com>
2024-04-22 17:42:12 -0500

    Implemented fetch motd endpoint testing

--- apps/express-backend/__tests__/motd/fetchMotd.test.ts
+++ apps/express-backend/__tests__/motd/fetchMotd.test.ts
@@ -1,5 +1,7 @@
 import { faker } from "@faker-js/faker";
+import { MessageOfTheDay } from "@motd-ts/models";
 import mongoose from "mongoose";
+import supertest from "supertest";
 import { afterAll, beforeAll, describe, expect, it } from "vitest";
 import { createMotd, fetchMotd } from "../../src/services/motd.services";
 import TestApp from "../utils/testApp";
@@ -45,5 +47,49 @@ describe("fetchMotd", () => {
       expect(foundMotd?.createdAt).toEqual(newMotd?.createdAt);
       expect(foundMotd?.updatedAt).toEqual(newMotd?.updatedAt);
     }
+  });
+});
+
+describe("GET `/:motdId`", () => {
+  let testApp: TestApp;
+  beforeAll(async () => {
+    testApp = new TestApp();
+    await testApp.setup();
+  });
+  afterAll(async () => {
+    await testApp.teardown();
+  });
+
+  //
+  //  Tests
+  //
+
+  it("400 when invalid ID used", async () => {
+    const response = await supertest(testApp.server).get(`/BADIDTHINGY`);
+    expect(response.statusCode).toEqual(400);
+
+    const foundMotd: MessageOfTheDay = response.body;
+    expect(foundMotd._id).toBeFalsy();
+  });
+
+  it("404 when non-existing ObjectId used", async () => {
+    const response = await supertest(testApp.server).get(`/${new mongoose.Types.ObjectId()}`);
+    expect(response.statusCode).toEqual(404);
+
+    const foundMotd: MessageOfTheDay = response.body;
+    expect(foundMotd._id).toBeFalsy();
+  });
+
+  it("200 when MOTD exists", async () => {
+    const motd = await createMotd(faker.hacker.phrase());
+    const response = await supertest(testApp.server).get(`/${motd?._id}`);
+    expect(response.statusCode).toEqual(200);
+
+    const transformedMotd = JSON.parse(JSON.stringify(motd)) as MessageOfTheDay;
+    const foundMotd: MessageOfTheDay = response.body;
+    expect(transformedMotd._id).toEqual(foundMotd._id);
+    expect(transformedMotd.message).toEqual(foundMotd.message);
+    expect(transformedMotd.createdAt).toEqual(foundMotd.createdAt);
+    expect(transformedMotd.updatedAt).toEqual(foundMotd.updatedAt);
   });
 });

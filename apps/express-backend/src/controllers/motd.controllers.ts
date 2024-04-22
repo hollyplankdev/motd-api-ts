91d096b99a7023369ba71cb17ebc5902a66d591a
HollyGrahamPlank <hollygrahamplank@gmail.com>
2024-04-22 13:54:12 -0500

    Remove works!

--- apps/express-backend/src/controllers/motd.controllers.ts
+++ apps/express-backend/src/controllers/motd.controllers.ts
@@ -2,17 +2,17 @@ import { RequestHandler } from "express";
 import { MessageOfTheDayModel } from "../models/messageOfTheDay";
 
 export const create: RequestHandler = async (req, res) => {
-  // Use the given message to create a new MOTD object
+  console.log(`Trying create MOTD...`);
   const motd = await MessageOfTheDayModel.create({ message: req.body.message });
-  console.log(`Created MOTD w/ id ${motd._id}`);
 
   // Turn into an object without the verison key and SEND!
   res.status(200).send(motd.toObject({ versionKey: false }));
+  console.log(`...Created MOTD w/ id ${motd._id}`);
 };
 
 export const readLatest: RequestHandler = async (req, res) => {
+  console.log(`Trying read latest MOTD...`);
   const motd = await MessageOfTheDayModel.findOne().sort({ createdAt: "descending" });
-  console.log(`Read latest MOTD (${motd?._id})`);
 
   // If there's no MOTD... EXIT EARLY!
   if (!motd) {
@@ -22,11 +22,12 @@ export const readLatest: RequestHandler = async (req, res) => {
 
   // Turn into an object without the version key and SEND!
   res.status(200).send(motd.toObject({ versionKey: false }));
+  console.log(`...Read latest MOTD (${motd?._id})`);
 };
 
 export const read: RequestHandler = async (req, res) => {
+  console.log(`Trying read MOTD w/ id ${req.params.id}...`);
   const motd = await MessageOfTheDayModel.findById(req.params.id);
-  console.log(`Read MOTD w/ id ${req.params.id}`);
 
   // If there's no MOTD... EXIT EARLY!
   if (!motd) {
@@ -36,9 +37,11 @@ export const read: RequestHandler = async (req, res) => {
 
   // Turn into an object without the version key and SEND!
   res.status(200).send(motd.toObject({ versionKey: false }));
+  console.log(`...Read MOTD w/ id ${req.params.id}`);
 };
 
 export const update: RequestHandler = async (req, res) => {
+  console.log(`Trying update MOTD w/ id ${req.params.id}...`);
   const motd = await MessageOfTheDayModel.findById(req.params.id);
 
   // If there's no MOTD... EXIT EARLY!
@@ -51,6 +54,20 @@ export const update: RequestHandler = async (req, res) => {
   motd.message = req.body.message;
   await motd.save();
   res.status(200).send(motd.toObject({ versionKey: false }));
+  console.log(`...Updated MOTD w/ id ${req.params.id}...`);
 };
 
-export const remove: RequestHandler = async (req, res) => {};
+export const remove: RequestHandler = async (req, res) => {
+  console.log(`Trying remove MOTD w/ id ${req.params.id}...`);
+  const result = await MessageOfTheDayModel.deleteOne({ _id: req.params.id });
+
+  // If there's no MOTD... EXIT EARLY!
+  if (result.deletedCount <= 0) {
+    res.status(404).send();
+    return;
+  }
+
+  // OTHERWISE - we did delete one!
+  res.status(200).send();
+  console.log(`...Removed MOTD w/ id ${req.params.id}`);
+};

4c93c143fffcb6a13b37dabf01fc3031efbde4fa
HollyGrahamPlank <hollygrahamplank@gmail.com>
2024-04-22 17:17:00 -0500

    Added endpoint tests to fetchLatest

--- apps/express-backend/__tests__/motd/fetchLatestMotd.test.ts
+++ apps/express-backend/__tests__/motd/fetchLatestMotd.test.ts
@@ -1,7 +1,9 @@
 import { faker } from "@faker-js/faker";
-import { afterEach, beforeEach, describe, expect, it } from "vitest";
+import { afterEach, beforeEach, describe, expect, it, test } from "vitest";
 import { createMotd, fetchLatestMotd } from "../../src/services/motd.services";
 import TestApp from "../utils/testApp";
+import supertest from "supertest";
+import { MessageOfTheDay } from "@motd-ts/models";
 
 describe("fetchLatestMotd", () => {
   let testApp: TestApp;
@@ -38,5 +40,38 @@ describe("fetchLatestMotd", () => {
     expect(latestMotd?.message).not.toEqual(newMotd?.message);
     expect(latestMotd?.createdAt).not.toEqual(newMotd?.createdAt);
     expect(latestMotd?.updatedAt).not.toEqual(newMotd?.updatedAt);
+  });
+});
+
+describe("GET `/`", () => {
+  let testApp: TestApp;
+  beforeEach(async () => {
+    testApp = new TestApp();
+    await testApp.setup();
+  });
+  afterEach(async () => {
+    await testApp.teardown();
+  });
+
+  //
+  //  Tests
+  //
+
+  it("200 when MOTD exists", async () => {
+    const motd = await createMotd(faker.hacker.phrase());
+    const response = await supertest(testApp.server).get("/");
+    expect(response.statusCode).toEqual(200);
+
+    const transformedMotd = JSON.parse(JSON.stringify(motd)) as MessageOfTheDay;
+    const foundMotd: MessageOfTheDay = response.body;
+    expect(transformedMotd._id).toEqual(foundMotd._id);
+    expect(transformedMotd.message).toEqual(foundMotd.message);
+    expect(transformedMotd.createdAt).toEqual(foundMotd.createdAt);
+    expect(transformedMotd.updatedAt).toEqual(foundMotd.updatedAt);
+  });
+
+  it("404 when no MOTDs", async () => {
+    const response = await supertest(testApp.server).get("/");
+    expect(response.statusCode).toEqual(404);
   });
 });

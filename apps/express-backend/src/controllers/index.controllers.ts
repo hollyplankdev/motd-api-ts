92f5e760dd70a499c80b6982da39154e2bf1be50
HollyGrahamPlank <hollygrahamplank@gmail.com>
2024-04-22 12:17:15 -0500

    Setup shared model package

--- apps/express-backend/src/controllers/index.controllers.ts
+++ apps/express-backend/src/controllers/index.controllers.ts
@@ -1,7 +1,8 @@
 import { RequestHandler } from "express";
 
 export const create: RequestHandler = async (req, res) => {
-  console.log(`CREATE:\n\t${req.body}`);
+  console.log(`CREATE:\n\t${JSON.stringify(req.body)}`);
+  res.status(200).send(req.body);
 };
 
 export const readLatest: RequestHandler = async (req, res) => {};
---
+++ packages/models/src/index.ts
@@ -0,0 +1,2 @@
+const someText: string = "Hello World!";
+export default someText;

47ab5d16f33bcfa94807c4cf7c3720fc93b23ccc
HollyGrahamPlank <hollygrahamplank@gmail.com>
2024-04-23 16:53:47 -0500

    Installed modals

--- apps/react-frontend/src/App.tsx
+++ apps/react-frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
+import { ModalsProvider } from "@mantine/modals";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
 import LatestMotdDisplay from "./features/motd/LatestMotdDisplay";
@@ -11,13 +12,15 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <MantineProvider theme={theme} defaultColorScheme="dark">
-        <div className="App">
-          <div className="AppContainer">
-            <div className="CenterPanel">
-              <LatestMotdDisplay />
+        <ModalsProvider>
+          <div className="App">
+            <div className="AppContainer">
+              <div className="CenterPanel">
+                <LatestMotdDisplay />
+              </div>
             </div>
           </div>
-        </div>
+        </ModalsProvider>
       </MantineProvider>
     </QueryClientProvider>
   );

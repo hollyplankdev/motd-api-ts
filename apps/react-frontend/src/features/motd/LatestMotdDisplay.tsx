b2ad90c4d272412c1112eedb849e119d1f11eeee
Holly Plank <hollyplankdev@gmail.com>
2024-04-25 14:38:56 -0500

    Simplified latest display a bit

--- apps/react-frontend/src/features/motd/LatestMotdDisplay.tsx
+++ apps/react-frontend/src/features/motd/LatestMotdDisplay.tsx
@@ -5,17 +5,17 @@ import { MotdDisplay, MotdDisplayProps } from "./MotdDisplay";
 export default function LatestMotdDisplay() {
   const query = useQuery({ queryKey: ["motd", "latest"], queryFn: getLatestMotd });
 
-  const getLoadStateFromQuery = (): MotdDisplayProps["loadState"] => {
-    if (query.isError) return "error";
-    if (query.isPending) return "loading";
-    return "done";
-  };
+  // Distill the load state from the query's ACTUAL loading state
+  let loadState: MotdDisplayProps["loadState"];
+  if (query.isError) {
+    loadState = "error";
+  } else if (query.isPending) {
+    loadState = "loading";
+  } else {
+    loadState = "done";
+  }
 
   return (
-    <MotdDisplay
-      motd={query.data}
-      loadState={getLoadStateFromQuery()}
-      errorMessage={query.error?.toString()}
-    />
+    <MotdDisplay motd={query.data} loadState={loadState} errorMessage={query.error?.toString()} />
   );
 }

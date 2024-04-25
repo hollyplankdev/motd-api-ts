ed731a3e4af0ae4be587d6c8b6983d8544910f7d
Holly Plank <hollyplankdev@gmail.com>
2024-04-25 17:21:25 -0500

    Cleaned create new button

--- apps/react-frontend/src/features/history/HistoryPanel.tsx
+++ apps/react-frontend/src/features/history/HistoryPanel.tsx
@@ -1,10 +1,10 @@
-import { Button, Drawer, Group, Modal, Stack } from "@mantine/core";
+import { Button, Drawer, Group, Modal, Paper, Stack } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { useQuery } from "@tanstack/react-query";
 import { getAllMotdHistory } from "../../api/motd";
 import { MotdDisplay } from "../motd/MotdDisplay";
-import styles from "./HistoryPanel.module.css";
 import { NewMotdForm } from "../motd/NewMotdForm";
+import styles from "./HistoryPanel.module.css";
 
 export default function HistoryPanel() {
   const [isHistoryOpen, { open: openHistory, close: closeHistory }] = useDisclosure(false);
@@ -24,10 +24,12 @@ export default function HistoryPanel() {
         size="lg"
         title="MOTD History"
       >
-        <Stack>
-          <Group justify="end">
-            <Button onClick={openCreateNew}>Create New</Button>
-          </Group>
+        <Stack gap="xs">
+          <Paper shadow="lg" radius="md" style={{ padding: "10px" }}>
+            <Group justify="end">
+              <Button onClick={openCreateNew}>Create New</Button>
+            </Group>
+          </Paper>
           {query.data.map((motd) => (
             <MotdDisplay key={motd._id} motd={motd} loadState="done" isEditable size="small" />
           ))}

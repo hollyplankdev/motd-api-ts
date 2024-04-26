8f992157eefb5c6d35858f3612e737f80e228be8
HollyGrahamPlank <hollygrahamplank@gmail.com>
2024-04-26 15:08:01 -0500

    Login button works!

--- apps/react-frontend/src/features/account/AccountPanel.tsx
+++ apps/react-frontend/src/features/account/AccountPanel.tsx
@@ -1,11 +1,14 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Button, Stack } from "@mantine/core";
 import styles from "./AccountPanel.module.css";
 
 export default function AccountPanel() {
+  const { loginWithRedirect } = useAuth0();
+
   return (
     <div className={styles.container}>
       <Stack className={styles.panelButton}>
-        <Button>Login</Button>
+        <Button onClick={() => loginWithRedirect()}>Log In</Button>
       </Stack>
     </div>
   );

57539d727eb79f712abc544f9e3911c12701e055
Holly Plank <hollyplankdev@gmail.com>
2024-06-10 16:43:59 -0500

    Separated fetchMotd

--- apps/express-backend/__tests__/motd/fetchMotd.unit.test.ts
+++ apps/express-backend/__tests__/motd/fetchMotd.unit.test.ts
@@ -1,5 +1,4 @@
 import { faker } from "@faker-js/faker";
-import { MessageOfTheDay } from "@motd-ts/models";
 import mongoose from "mongoose";
 import { afterAll, beforeAll, describe, expect, it } from "vitest";
 import { createMotd, fetchMotd } from "../../src/services/motd.services";
@@ -46,48 +45,5 @@ describe("fetchMotd", () => {
       expect(foundMotd?.createdAt).toEqual(newMotd?.createdAt);
       expect(foundMotd?.updatedAt).toEqual(newMotd?.updatedAt);
     }
-  });
-});
-
-describe("GET `/:motdId`", () => {
-  let testApp: TestApp;
-  beforeAll(async () => {
-    testApp = new TestApp();
-    await testApp.setup({ defaultAudience: "/motd" });
-  });
-  afterAll(async () => {
-    await testApp.teardown();
-  });
-
-  //
-  //  Tests
-  //
-
-  it("400 when invalid ID used", async () => {
-    const response = await testApp.api.getMotd("INVALIDID").expect(400);
-
-    const foundMotd: MessageOfTheDay = response.body;
-    expect(foundMotd._id).toBeFalsy();
-  });
-
-  it("404 when non-existing ObjectId used", async () => {
-    const response = await testApp.api
-      .getMotd(new mongoose.Types.ObjectId().toString())
-      .expect(404);
-
-    const foundMotd: MessageOfTheDay = response.body;
-    expect(foundMotd._id).toBeFalsy();
-  });
-
-  it("200 when MOTD exists", async () => {
-    const motd = await createMotd(faker.hacker.phrase());
-    const response = await testApp.api.getMotd(motd?._id!).expect(200);
-
-    const transformedMotd = JSON.parse(JSON.stringify(motd)) as MessageOfTheDay;
-    const foundMotd: MessageOfTheDay = response.body;
-    expect(transformedMotd._id).toEqual(foundMotd._id);
-    expect(transformedMotd.message).toEqual(foundMotd.message);
-    expect(transformedMotd.createdAt).toEqual(foundMotd.createdAt);
-    expect(transformedMotd.updatedAt).toEqual(foundMotd.updatedAt);
   });
 });
